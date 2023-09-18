e265a2ed5740a6429faeac51a158c4b2e7d8f7c7
enyllaoliveira <enyllaoliveira@gmail.com>
2023-09-18 10:45:08 -0300

    building the page.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-//   return (
-//     <div>
-//       <h1>Meus dados da API</h1>
-//       <ul>
-//         {dadosAPI.map((item, index) => (
-//           <li key={index}>{item.nome}</li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
-
-
-import { useEffect, useState } from "react";
+import { useEffect, useState } from "react"
+import React from "react"
+import Booking from "./shared/components/booking/booking.jsx"
+import './App.module.css'
 
 export default function App() {
-  const [dadosAPI, setDadosAPI] = useState([]);
+  const [data, setData] = useState([])
+
+  const getData = async () => {
+    try {
+      const response = await fetch('https://course-api.com/react-tours-project')
+      const result = await response.json()
+      setData(result)
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   useEffect(() => {
-    fetch('https://course-api.com/react-tours-project')
-      .then((response) => response.json())
-      .then((dados) => setDadosAPI(dados))
-      .catch((erro) => console.error("Erro ao buscar dados da API", erro));
-  }, []);
+      getData()
+    }, [])
+
 
   return (
-    <div>
-      {dadosAPI.map((item, index) => (
-        <h1 key={index}>{item.nome}</h1>
-      ))}
+    <div className="container">
+      <h1> Nossas acomodações </h1>
+      <div className="booking-dados"> 
+      <Booking data={data}/>
+      </div>
     </div>
-  );
+    
+  )
 }
